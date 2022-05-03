fc2e26a7ec61a31495836fa32eb0464cb25e88c1
kevin860104 <55239515+kevin860104@users.noreply.github.com>
2022-05-03 14:51:22 +0800

    Add files via upload

---
+++ mytable.php
@@ -0,0 +1,24 @@
+<?php
+require 'connect.php';
+try {
+    $sql = "CREATE TABLE mytable (
+    ID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(50) NOT NULL,
+    Account VARCHAR(50) NOT NULL,
+    PhoneNumber VARCHAR(10) NOT NULL,
+    Password VARCHAR(100) NOT NULL,
+    Latitude float(50) NOT NULL,
+    Longitude float(50) NOT NULL
+    )";
+
+    $db->exec($sql);
+    echo "資料表 myable 建立成功";
+}
+
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}
+ 
+$conn = null;
+?>
