c9cf8b7f8bbb2791a667c8d1811299949e4e760a
0xBE61A55F <84661746+0xBE61A55F@users.noreply.github.com>
2022-05-06 00:47:28 +0800

    Delete connect.php

--- connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Content-type: text/html;charset=utf-8");
-$dbh = new PDO("mysql:host=localhost;dbname=test", "root", "");
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
